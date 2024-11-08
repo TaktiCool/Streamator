20384816336d873f9617cd2ed65f5b14e041d7da
jokoho48 <hoffmann.jonas95@gmail.com>
2024-11-08 20:44:58 +0100

    Bump Version

--- addons/Streamator/macros.hpp
+++ addons/Streamator/macros.hpp
@@ -4,9 +4,9 @@
 
 // Define version information
 #define MAJOR 0
-#define MINOR 6
+#define MINOR 7
 #define PATCHLVL 0
-#define BUILD 374
+#define BUILD 404
 
 // Do not release with this setting enabled!
 // #define DEBUGFULL // Enable all debug methods
